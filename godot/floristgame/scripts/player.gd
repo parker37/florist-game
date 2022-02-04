531aadc8d4124f50207a7dad282c63618d6faf18
Cherish <oelslagerc@yahoo.com>
2022-02-03 22:45:34 -0500

    here it is parker

--- godot/floristgame/scripts/player.gd
+++ godot/floristgame/scripts/player.gd
@@ -10,20 +10,24 @@ func read_input():
 		velocity.y -= 1
 		direction = Vector2(0,-1)
 
-	if Input.is_action_pressed("down"):
+	elif Input.is_action_pressed("down"):
 		velocity.y += 1
+		$AnimationPlayer.play("walk_down")
 		direction = Vector2(0,1)
 
-	if Input.is_action_pressed("left"):
+	elif Input.is_action_pressed("left"):
 		velocity.x -= 1
 		direction = Vector2(-1,0)
 
-	if Input.is_action_pressed("right"):
+	elif Input.is_action_pressed("right"):
 		velocity.x += 1
 		direction = Vector2(1,0)
+		
+	else:
+		$AnimationPlayer.play("idle")
 	
 	velocity = velocity.normalized()
-	velocity = move_and_slide(velocity * 200)
+	velocity = move_and_slide(velocity * 150)
 
 func _physics_process(delta):
 	read_input()
